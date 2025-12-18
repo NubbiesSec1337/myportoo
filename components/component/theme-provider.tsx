72038f07d9f6ec206d53e4009c7175bfe4b3bc1a
Bumz Mac <bumzmac@192.168.1.108>
2025-12-18 17:06:56 +0700

    Fix ThemeProvider and remove submodule

--- components/component/theme-provider.tsx
+++ components/component/theme-provider.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import { ReactNode } from 'react'
+import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider({ children }: { children: ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  )
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
